55e686984722bf41a3bcaae6b0022111cdcce211
LMarinelli <Lucamarinelli1940@gmail.com>
2023-09-28 22:12:43 -0400

    Added Library Successfully

--- TelemetryProcessor/TelemetryProcessor.cpp
+++ TelemetryProcessor/TelemetryProcessor.cpp
@@ -1,10 +1,18 @@
 // TelemetryProcessor.cpp
 #include <iostream>
-#include <cstring>
-// #include <sys/socket.h>
-#include <winsock2.h>
-#include <netinet/in.h> // don't have this library installed
-#include <unistd.h> // don't have this library installed
+#include <cstring>	
+#include <nlohmann/json.hpp> 
+/*
+	Steps to Add Library
+	____________________
+	- Download the Library off Github
+	- Open IDE (I used VS I don't know if its the same for any other)
+	- Open Solution Explorer
+	- Right-Click on project's name and select "properties"
+	- Navigate to Configuration Properties > VC++ Directories
+	- In "Include Directories" field, add the path ot the `include` folder that was extracted from Github
+*/
+using json = nlohmann::json;
 #define PORT 9000 // TCP Server -- listening on port 9000
 
 
