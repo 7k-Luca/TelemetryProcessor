2a92dd39426ba51e5eb1456bca0bd4a885e0f2a5
LMarinelli <Lucamarinelli1940@gmail.com>
2023-10-02 17:41:23 -0400

    Initialized sockpp, Created TCP Connector, Connected Server, Created JSON object for handshake, and Serialized the JSON object to string

--- TelemetryProcessor/TelemetryProcessor.cpp
+++ TelemetryProcessor/TelemetryProcessor.cpp
@@ -1,36 +1,37 @@
 // TelemetryProcessor.cpp
+using namespace std;
 #include <iostream>
 #include <cstring>	
-using namespace std;
-#include <nlohmann/json.hpp> 
-/*
-	Steps to Add Library
-	____________________
-	- Download the Library off Github
-	- Open IDE (I used VS I don't know if its the same for any other)
-	- Open Solution Explorer
-	- Right-Click on project's name and select "properties"
-	- Navigate to Configuration Properties > VC++ Directories
-	- In "Include Directories" field, add the path ot the `include` folder that was extracted from Github
-*/
+#include "sockpp/socket.h"
+#include "sockpp/connector.h"
+#include <nlohmann/json.hpp>
+
 using json = nlohmann::json;
-#define PORT 9000 // TCP Server -- listening on port 9000
 
 
 int main() {
-	// Example of json data
-	json j = {
-	   {"name", "John"},
-	   {"age", 30}
-	};
+	// Initialize the sockpp library
+	sockpp::initialize();
+	
+	try {
+		// Create a TCP connector
+		sockpp::tcp_connector conn; // error ~~ not identifying tcp_connector
 
-	// Example of printing that data
-	cout << j.dump() << endl;
+		// Connect to the server, localhost is tentative to change
+		conn.connect_tcp("localhost", 9000);
 
-}
+		// Create a JSON object for the handshake data
+		json handshake;
+		handshake["Length"] = static_cast<int32_t>(5); // Length of Data
+		handshake["Data"] = "START"; // Data
+		handshake["Field Type"] = "byte"; // Field Types
 
-// steps for server = Socket -> setSockopt -> Bind -> Listen -> Accept -> Send/Receive
-// function designed for chat between client and server
-void serv(int connfd) {
+		// Serialize the JSON object to a string
+		string jsonStr = handshake.dump()
+
+		// Send the handshake packet to the server
+	}
+
+	
 
 }
