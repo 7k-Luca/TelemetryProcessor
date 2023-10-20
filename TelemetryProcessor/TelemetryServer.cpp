a0723a3c5523d13998ded2503e97a81f5cd19829
yakubmungai <yakub.mungai@ufl.edu>
2023-10-20 19:28:14 -0400

    Created the telemetry server that accepts connection from client

---
+++ TelemetryProcessor/TelemetryServer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "sockpp/socket.h"
+#include "sockpp/tcp_acceptor.h"
+
+int main() {
+	int16_t port = 9000;  // Stores the port number which server will listen for incoming connections
+	sockpp::tcp_acceptor acc(port); // Creates an instance of tcp_acceptor
+	while (true) {
+			sockpp::tcp_socket sock = acc.accept(); // Incoming connections accepted (socket created to communicate with client)
+
+			if (!sock) { // Checking for errors during connection acceptance
+				cerr << "Error accepting incoming connection: "
+					<< acc.last_error_str() << endl;
+
+			else {
+				thread thr(ProcessTelemetry, std::move(sock)); // Creaate a thread to handle communication with client, allows multiple clients to be handled at once
+				thr.detach();
+			
+			}
+
+			}
+		}
+	}
+}
